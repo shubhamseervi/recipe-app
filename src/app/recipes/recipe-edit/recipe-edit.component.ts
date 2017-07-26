5d4fee7213028eca5480c45673c5ae894cae49af
Shubhanseervi <shubhamseervi@gmail.com>
2017-07-26 18:46:52 +0530

    RA: Added remove ingredient functionality

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -63,6 +63,11 @@ export class RecipeEditComponent implements OnInit {
       this.router.navigate(['../'], {relativeTo: this.route });
     }
 
+    onDeleteIngredient(index: number) {
+        (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+
+    }
+
     private initForm() {
 
       let recipeName = '';
